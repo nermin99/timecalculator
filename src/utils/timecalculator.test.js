eb05a2159b9efce147790880fafa101d7658a6ae
Nermin Skenderovic <nermin@skenderovic.se>
2022-03-31 18:05:48 +0200

    Add non-passing test

--- src/utils/timecalculator.test.js
+++ src/utils/timecalculator.test.js
@@ -4,6 +4,15 @@ test('testing >', () => {
   expect(evalExpr('08:00 > 09:30')).toBe('1 hour 30 minutes')
 })
 
+test('testing +', () => {
+  expect(evalExpr('08:00 + 1h30m')).toBe('09:30')
+})
+
 test('should wrap around 24h', () => {
   expect(evalExpr('23:00 + 2h')).toBe('01:00')
 })
+
+// TODO: returns '0 hours 0 minutes 0 seconds' atm
+// test('should test > priority', () => {
+//   expect(evalExpr('08:00 > 09:00 + 10:00 > 11:00')).toBe('2 hours')
+// })
