d95484d8ab316e43c92547e6663dfce813a17fb2
Nermin Skenderovic <nermin@skenderovic.se>
2022-10-28 14:30:05 +0200

    Add ribbon

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Calculator from './components/calculator'
 function App() {
   return (
     <div className="App">
+      <a href="https://github.com/nermin99/timecalculator" className="ribbon">
+        Check me out on GitHub!
+      </a>
       <Calculator />
     </div>
   )
